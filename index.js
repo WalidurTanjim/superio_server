f20da31c7ed15e50d3b77e43c05ec7e6175d43e8
WalidurTanjim <mohammadtanjim91@gmail.com>
2024-12-17 21:04:51 +0600

    token created & set to cookie

--- index.js
+++ index.js
@@ -1,15 +1,21 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const jwt = require('jsonwebtoken');
+const cookieParser = require('cookie-parser');
 const cloudinary = require('./cloudinary/cloudinary');
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 // middlewares
-app.use(cors());
+app.use(cors({
+  origin: 'http://localhost:5173',
+  credentials: true
+}));
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(cookieParser());
 
 
 
@@ -34,6 +40,16 @@ async function run() {
     const categoriesCollection = client.db('Superio').collection('categories');
     const jobsCollection = client.db('Superio').collection('jobs');
 
+
+    // jwt related api
+    app.post('/createToken', async(req, res) => {
+      const userInfo = req.body;
+      const token = jwt.sign(userInfo, process.env.TOKEN_SECRET, { expiresIn: '1h' });
+      res.cookie('token', token, {
+          httpOnly: true,
+          secure: false
+        }).send({ success: true })
+    })
 
 
     // categories related api
